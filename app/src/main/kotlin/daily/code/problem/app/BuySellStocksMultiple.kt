de1a84d31a943b70d31888fe6e2d2e6c988ae6cc
Vermatron <contact.ashitverma@gmail.com>
2025-05-05 17:26:10 +0100

    cleanup

--- app/src/main/kotlin/daily/code/problem/app/BuySellStocksMultiple.kt
+++ app/src/main/kotlin/daily/code/problem/app/BuySellStocksMultiple.kt
@@ -4,8 +4,6 @@ package daily.code.problem.app
 class BuySellStocksMultiple {
     fun maxProfit(prices: IntArray): Int {
         var profit = 0;
-        val dd = "ffff"
-        dd.toCharArray()[0].isLetter()
         for (i in 0 until prices.size - 1){
             if (prices[i + 1] > prices[i]){
                 profit += (prices[i + 1] - prices[i])
