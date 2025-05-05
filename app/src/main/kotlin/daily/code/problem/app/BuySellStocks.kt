ed786486a9b52c60f8996c06ff443aa59ecc993a
Vermatron <contact.ashitverma@gmail.com>
2025-05-05 16:43:31 +0100

    buy or sell stocks

---
+++ app/src/main/kotlin/daily/code/problem/app/BuySellStocks.kt
@@ -0,0 +1,21 @@
+package daily.code.problem.app
+
+class BuySellStocks {
+    fun maxProfit(prices: IntArray): Int {
+        var minPrice = Int.MAX_VALUE
+        var maxProfit = 0
+
+        for (price in prices) {
+            if (price < minPrice) {
+                minPrice = price // found a lower buying price
+            } else {
+                val profit = price - minPrice
+                if (profit > maxProfit) {
+                    maxProfit = profit // found a better profit
+                }
+            }
+        }
+
+        return maxProfit
+    }
+}
